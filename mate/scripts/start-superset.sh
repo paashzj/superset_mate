2c431694e815327fdcec634b7ad73c0991178ede
shoothzj <shoothzj@gmail.com>
2021-08-25 13:41:11 +0800

    change stdout log

--- mate/scripts/start-superset.sh
+++ mate/scripts/start-superset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup superset run -h 0.0.0.0 -p 8088 --reload --debugger >$SUPERSET_HOME/superset_mate.log 2>$SUPERSET_HOME/superset_mate_error.log &
+nohup superset run -h 0.0.0.0 -p 8088 --reload --debugger >$SUPERSET_HOME/superset.stdout.log 2>$SUPERSET_HOME/superset.stderr.log &
