dd6be891c63f207e98eb409318f443c3d9da6dcd
shoothzj <shoothzj@gmail.com>
2021-09-02 19:26:39 +0800

    optimize boot

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 from flask import Flask
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 $SUPERSET_HOME/mate/app/app.py
+cd $SUPERSET_HOME/mate
+python3 $SUPERSET_HOME/mate/app/main.py  >>$SUPERSET_HOME/superset_mate.stdout.log 2>>$SUPERSET_HOME/superset_mate.stderr.log
+tail -f /dev/null
